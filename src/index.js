51d97d2aaac09640a17189efde1f0af628fee1af
francisco javier florez ramirez <franciscojflorezr12@gmail.com>
2023-07-30 16:54:46 -0500

    prueba documentacion con swagger

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const v1Router = require('./v1/routes');
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const apicache = require('apicache');
+const { swaggerDocs: V1SwaggerDocs} = require('./v1/swagger');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,4 +14,5 @@ app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT)
 });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const apicache = require('apicache');
+const apicache = require("apicache");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordControllers");
 
@@ -7,7 +7,45 @@ const router = express.Router();
 const cache = apicache.middleware;
 
 router
-  .get("/", cache("2 minutes"), workoutController.getAllWorkouts)
+  /**
+   * @openapi
+   * /api/v1/workouts:
+   *   get:
+   *     tags:
+   *       - Workouts
+   *     responses:
+   *       200:
+   *         description: OK
+   *         content:
+   *           application/json:
+   *               schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   example: OK
+   *                 data:
+   *                   type: array
+   *                   items:
+   *                      $ref: "#/components/schemas/Workout"
+   *       5XX:
+   *         description: FAILED
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   example: FAILED
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     error:
+   *                       type: string
+   *                       example: "Some error message"
+   */
+  .get("/", workoutController.getAllWorkouts)
 
   .get("/:workoutId", workoutController.getOneWorkout)
 
