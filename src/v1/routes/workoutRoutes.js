126155fd8afc74bbfef055bfd0e4f2d3a661acad
francisco javier florez ramirez <franciscojflorezr12@gmail.com>
2023-07-30 15:30:36 -0500

    api crossfit

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const workoutController = require("../../controllers/workoutController");
+const recordController = require("../../controllers/recordControllers");
+
+router
+  .get("/", workoutController.getAllWorkouts)
+
+  .get("/:workoutId", workoutController.getOneWorkout)
+
+  .get("/:workoutId/records", recordController.getRecordForWorkout)
+
+  .post("/", workoutController.createNewWorkout)
+
+  .patch("/:workoutId", workoutController.updateOneWorkout)
+
+  .delete("/:workoutId", workoutController.deleteOneWorkout);
+
+module.exports = router;
